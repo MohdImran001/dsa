ff3edbebba87d4d855738d6f7be313a3c8ac666e
Mohd Imran <42844688+MohdImran001@users.noreply.github.com>
2020-06-08 12:27:19 +0530

    Create count-number-of-strings.cpp

---
+++ count-number-of-strings.cpp
@@ -0,0 +1,22 @@
+int count_DP(int n) {
+    int dp[n+1];
+    dp[0] = 0;
+    dp[1] = 2;
+    dp[2] = 3;
+
+    for(int i=3; i<=n; ++i) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        cout << count_DP(n) << "\n";
+    }
+}
